9514f0a12f01a8de31aa0a962db89ed512825b47
spsbs <spsbs@DESKTOP-60A7QSE>
2021-10-22 10:30:41 +0900

    설정추가

--- WhatsTodo/src/main/java/model/Todo/TodoVO.java
+++ WhatsTodo/src/main/java/model/Todo/TodoVO.java
@@ -1,5 +1,40 @@
 package model.Todo;
 
 public class TodoVO {
-
+	private int todoNum;
+	private String todo;
+	private String cid;
+	private boolean achieveTodo;
+	
+	// get set
+	public int getTodoNum() {
+		return todoNum;
+	}
+	public void setTodoNum(int todoNum) {
+		this.todoNum = todoNum;
+	}
+	public String getTodo() {
+		return todo;
+	}
+	public void setTodo(String todo) {
+		this.todo = todo;
+	}
+	public String getCid() {
+		return cid;
+	}
+	public void setCid(String cid) {
+		this.cid = cid;
+	}
+	public boolean isAchieveTodo() {
+		return achieveTodo;
+	}
+	public void setAchieveTodo(boolean achieveTodo) {
+		this.achieveTodo = achieveTodo;
+	}
+	
+	//toString
+	@Override
+	public String toString() {
+		return "TodoVO [todoNum=" + todoNum + ", todo=" + todo + ", cid=" + cid + ", achieveTodo=" + achieveTodo + "]";
+	}
 }
--- WhatsTodo/src/main/java/model/client/ClientVO.java
+++ WhatsTodo/src/main/java/model/client/ClientVO.java
@@ -1,5 +1,99 @@
 package model.client;
 
 public class ClientVO {
-
+	private String id;
+	private String pw;
+	private String name;
+	private boolean admin;
+	private int dayTodoCnt;
+	private int weekTodoCnt;
+	private int monthTodoCnt;
+	private int yearTodoCnt;
+	private int dayAchieve;
+	private int weekAchieve;
+	private int monthAchieve;
+	private int yearAchieve;
+	
+	// get, set
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public boolean isAdmin() {
+		return admin;
+	}
+	public void setAdmin(boolean admin) {
+		this.admin = admin;
+	}
+	public int getDayTodoCnt() {
+		return dayTodoCnt;
+	}
+	public void setDayTodoCnt(int dayTodoCnt) {
+		this.dayTodoCnt = dayTodoCnt;
+	}
+	public int getWeekTodoCnt() {
+		return weekTodoCnt;
+	}
+	public void setWeekTodoCnt(int weekTodoCnt) {
+		this.weekTodoCnt = weekTodoCnt;
+	}
+	public int getMonthTodoCnt() {
+		return monthTodoCnt;
+	}
+	public void setMonthTodoCnt(int monthTodoCnt) {
+		this.monthTodoCnt = monthTodoCnt;
+	}
+	public int getYearTodoCnt() {
+		return yearTodoCnt;
+	}
+	public void setYearTodoCnt(int yearTodoCnt) {
+		this.yearTodoCnt = yearTodoCnt;
+	}
+	public int getDayAchieve() {
+		return dayAchieve;
+	}
+	public void setDayAchieve(int dayAchieve) {
+		this.dayAchieve = dayAchieve;
+	}
+	public int getWeekAchieve() {
+		return weekAchieve;
+	}
+	public void setWeekAchieve(int weekAchieve) {
+		this.weekAchieve = weekAchieve;
+	}
+	public int getMonthAchieve() {
+		return monthAchieve;
+	}
+	public void setMonthAchieve(int monthAchieve) {
+		this.monthAchieve = monthAchieve;
+	}
+	public int getYearAchieve() {
+		return yearAchieve;
+	}
+	public void setYearAchieve(int yearAchieve) {
+		this.yearAchieve = yearAchieve;
+	}
+	
+	// toString
+	@Override
+	public String toString() {
+		return "ClientVO [id=" + id + ", pw=" + pw + ", name=" + name + ", admin=" + admin + ", dayTodoCnt="
+				+ dayTodoCnt + ", weekTodoCnt=" + weekTodoCnt + ", monthTodoCnt=" + monthTodoCnt + ", yearTodoCnt="
+				+ yearTodoCnt + ", dayAchieve=" + dayAchieve + ", weekAchieve=" + weekAchieve + ", monthAchieve="
+				+ monthAchieve + ", yearAchieve=" + yearAchieve + "]";
+	}
 }
