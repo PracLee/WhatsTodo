4ca5e0f8ef8fccbfc3caf091f1e987922e6d3f76
PracLee <mrgoga@naver.com>
2021-10-25 09:55:00 +0900

    table 작업 완료

---
+++ WhatsTodo/Test.sql
@@ -0,0 +1,29 @@
+-- C	
+create table Client(
+	id varchar(50) primary key,
+	pw varchar(100) not null,
+	name varchar(150) not null,
+	todoCnt int default 0,
+	achieveCnt int default 0	
+);
+create table Todo(
+	todonum int primary key,
+	todo varchar(500) not null,
+	cid varchar(50) not null,
+	achieveTodo int default 0 constraint todo_achievetodo_CK check(achieveTodo = 0 or achieveTodo = 1),	
+	toDate date default sysdate,
+	deadLine date default sysdate,
+	CONSTRAINT fk_id foreign key (cid) references Client(id) on delete cascade
+);
+-- sample
+insert into CLIENT(id, pw, name) values('admin', '1234', '관리자');
+insert into todo(todonum, todo, cid) values(0, '일하자', 'admin');
+
+
+-- R
+select * from CLIENT;
+select * from Todo;
+
+-- D
+drop table Client;
+drop table todo;
